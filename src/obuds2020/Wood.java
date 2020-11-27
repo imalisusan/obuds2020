5553581e2884f6bf6f9339372171d1e3552641ed
Imali Susan <susanimali52@gmail.com>
2020-11-26 17:02:42 -0800

    01_Initial Creation of Abstract class

---
+++ src/obuds2020/Wood.java
@@ -0,0 +1,226 @@
+package obuds2020;
+
+abstract class Wood {
+  // Abstract method (does not have a body)
+  public abstract void sales();
+  public abstract void stock();
+  public abstract void profit();
+  public abstract void totalprofit();
+  
+  //variables
+  private int price;
+  
+  // Regular method
+  public void sleep() {
+    System.out.println("Zzz");
+  }
+}
+
+// Subclass (inherit from Animal)
+class A extends Wood {
+  public void sales() {
+    
+    
+  }
+    @Override
+    public void stock() 
+    {
+       
+    }
+    @Override
+    public void profit() {
+        
+    }
+    @Override
+    public void totalprofit() {
+       
+    }
+}
+
+class B extends Wood {
+  public void sales() {
+    int price;
+    
+  }
+    @Override
+    public void stock() 
+    {
+       
+    }
+    @Override
+    public void profit() {
+        
+    }
+    @Override
+    public void totalprofit() {
+       
+    }
+}
+
+class C extends Wood {
+  public void sales() {
+    int price;
+    
+  }
+    @Override
+    public void stock() 
+    {
+       
+    }
+    @Override
+    public void profit() {
+        
+    }
+    @Override
+    public void totalprofit() {
+       
+    }
+}
+
+class D extends Wood {
+  public void sales() {
+    int price;
+    
+  }
+    @Override
+    public void stock() 
+    {
+       
+    }
+    @Override
+    public void profit() {
+        
+    }
+    @Override
+    public void totalprofit() {
+       
+    }
+}
+
+class E extends Wood {
+  public void sales() {
+    int price;
+    
+  }
+    @Override
+    public void stock() 
+    {
+       
+    }
+    @Override
+    public void profit() {
+        
+    }
+    @Override
+    public void totalprofit() {
+       
+    }
+}
+
+class F extends Wood {
+  public void sales() {
+    int price;
+    
+  }
+    @Override
+    public void stock() 
+    {
+       
+    }
+    @Override
+    public void profit() {
+        
+    }
+    @Override
+    public void totalprofit() {
+       
+    }
+}
+
+class G extends Wood {
+  public void sales() {
+    int price;
+    
+  }
+    @Override
+    public void stock() 
+    {
+       
+    }
+    @Override
+    public void profit() {
+        
+    }
+    @Override
+    public void totalprofit() {
+       
+    }
+}
+
+class H extends Wood {
+  public void sales() {
+    int price;
+    
+  }
+    @Override
+    public void stock() 
+    {
+       
+    }
+    @Override
+    public void profit() {
+        
+    }
+    @Override
+    public void totalprofit() {
+       
+    }
+}
+
+class I extends Wood {
+  public void sales() {
+    int price;
+    
+  }
+    @Override
+    public void stock() 
+    {
+       
+    }
+    @Override
+    public void profit() {
+        
+    }
+    @Override
+    public void totalprofit() {
+       
+    }
+}
+
+class J extends Wood {
+  public void sales() {
+    int price;
+    
+  }
+    @Override
+    public void stock() 
+    {
+       
+    }
+    @Override
+    public void profit() {
+        
+    }
+    @Override
+    public void totalprofit() {
+       
+    }
+}
+
+class MyMainClass {
+  public static void main(String[] args) {
+    A myWood = new A(); // Create a Wood object
+    myWood.sales();
+    myWood.sleep();
+  }
+}
